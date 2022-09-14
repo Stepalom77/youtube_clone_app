205c72fb5fc8bbcd6211565ab99fcd4f4c98846d
Stepalom77 <stepalom22@gmail.com>
2022-09-14 16:25:28 -0500

    user subscriptions added

--- src/components/UserSubscriptions.tsx
+++ src/components/UserSubscriptions.tsx
@@ -1,16 +1,35 @@
 import { ListGroup } from "flowbite-react";
 import React, {useState, useEffect} from "react";
-
+import axios from "axios";
+interface UserSubscriptions {
+    subscriptions: string[]
+  }
 const Subscriptions:React.FC = () => {
-    const [subscription, setSubscription] = useState('subscription')
+    const [subscription, setSubscription] = useState<UserSubscriptions>({subscriptions : []})
+    useEffect(() => {
+        const fetchUserSubscriptions = async() => {
+            try {
+                const response = await axios.get<UserSubscriptions>('http://localhost:7000/api/users/631fe4bfed9eb772e211481c')
+                setSubscription(response.data)
+            } catch(err) {
+                console.error(err)
+            }
+        }
+        fetchUserSubscriptions()
+    }, [])
     return(
-        <div className="w-48">
-        <ListGroup>
-            <ListGroup.Item>
-                {subscription}
-            </ListGroup.Item>
-  </ListGroup>
-</div>
+        <div className="ml-40 mt-10 mb-5 grid md:grid-cols-1 md:gap-6 max-w-6xl">
+            {subscription.subscriptions.map((res) => {
+                return(
+                    <ListGroup>
+                    <ListGroup.Item>
+                        {res}
+                    </ListGroup.Item>
+                </ListGroup>
+                )
+            })}
+
+        </div>
     )
 }
 
