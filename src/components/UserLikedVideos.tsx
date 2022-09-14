2d319d9a1d0c2beff6089d7aa36b73e7198a885d
Stepalom77 <stepalom22@gmail.com>
2022-09-14 16:18:02 -0500

    user liked videos get all added

--- src/components/UserLikedVideos.tsx
+++ src/components/UserLikedVideos.tsx
@@ -1,15 +1,34 @@
 import { Card } from "flowbite-react";
 import React, {useState, useEffect} from "react";
-
+import axios from "axios";
+interface UserLikedVideos {
+    liked_videos: string[]
+  }
 const LikedVideos:React.FC = () => {
-    const [likedVideo, setLikedVideo] = useState('video title')
+    const [user, setUser] = useState<UserLikedVideos>({liked_videos : []})
+    useEffect(() => {
+        const fetchUserLikedVideos = async() => {
+            try {
+                const response = await axios.get<UserLikedVideos>('http://localhost:7000/api/users/631fe4bfed9eb772e211481c')
+                setUser(response.data)
+            } catch(err) {
+                console.error(err)
+            }
+        }
+        fetchUserLikedVideos()
+    }, [])
     return(
-        <div className="max-w-sm">
-        <Card>
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {likedVideo}
-            </h5>
-        </Card>
+        <div className="ml-40 mt-10 mb-5 grid md:grid-cols-2 md:gap-6 max-w-6xl">
+            {user.liked_videos.map((res) => {
+                return(
+                    <Card>
+                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            {res}
+                        </h5>
+                </Card>
+                )
+            })}
+
     </div>
     )
 }
