ee6fb050e6c9f443b7f42025dda4820bfdc34fee
Stepalom77 <stepalom22@gmail.com>
2022-09-15 17:33:20 -0500

    Auth0 aditional  setup

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,8 @@ root.render(
     domain='dev-0zyvabu1.us.auth0.com'
     clientId='JUM8sDJZsCAfwdvT7A8ndV0jh1yLsYIT'
     redirectUri={window.location.origin}
+    audience="http://localhost:7000/"
+    scope='openid profile email'
     >
     <App />
     </Auth0Provider>
