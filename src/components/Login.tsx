6217e633b64c460d19990af49e4b3e096b866100
Stepalom77 <stepalom22@gmail.com>
2022-09-15 15:48:46 -0500

    Auth0 login added

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,28 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Login: React.FC = () => {
+    const {loginWithPopup, loginWithRedirect, logout, user, isAuthenticated} = useAuth0()
     return(
-        <div></div>
+        <div>
+            <h1>Auth0 Login</h1>
+            <ul>
+                <li>
+                    <button onClick={loginWithPopup}>Login with Popup</button>
+                </li>
+                <li>
+                    <button onClick={loginWithRedirect}>Login with Redirect</button>
+                </li>
+                <li>
+                    <button onClick={logout as any}>Logout</button>
+                </li>
+            </ul>
+            <h3>User is {isAuthenticated ? "Logged in" : "Not logged in"}</h3>
+            {isAuthenticated && (
+                <pre style={{textAlign: 'start'}}>{JSON.stringify(user, null, 2)}</pre>
+            )}
+        </div>
     )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,12 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain='dev-0zyvabu1.us.auth0.com'
+    clientId='JUM8sDJZsCAfwdvT7A8ndV0jh1yLsYIT'
+    redirectUri={window.location.origin}>
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
