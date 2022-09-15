49c8421ba2684e5a78d98f731baf43b2eb4229d9
Stepalom77 <stepalom22@gmail.com>
2022-09-15 14:26:03 -0500

    done for now

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -19,9 +19,9 @@ const NavBar:React.FC = () => {
                         <li>
                             <a href="/user" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">User</a>
                         </li>
-                        <li>
+                       {/* <li>
                             <a href="/sign" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign up</a>
-                        </li>
+                        </li>*/}
                         <li>
                             <a href="/comments" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Comments</a>
                         </li>
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,43 +1,200 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import { Button, Label, TextInput } from "flowbite-react";
+
+interface User {
+    email: string,
+    username: string,
+    description: string,
+    first_name: string,
+    last_name: string,
+    telephone_number:any,
+    payment_method: string,
+    rating: string
+  }
 
 const SignIn: React.FC = () => {
+
+    const [data, setData] = useState<User>({
+            email: '',
+            username: '',
+            description: '',
+            first_name: '',
+            last_name: '',
+            telephone_number: 0,
+            payment_method: '',
+            rating: ''
+        })
+
+    const handleSubmit = async(e:any) => {
+        //e.preventDefault()
+        const userFormData = new FormData();
+        userFormData.append("email", data.email)
+        userFormData.append("username", data.username)
+        userFormData.append("description", data.description)
+        userFormData.append("first_name", data.first_name)
+        userFormData.append("last_name", data.last_name)
+        userFormData.append("telephone_number", data.telephone_number)
+        userFormData.append("payment_method", data.payment_method)
+        userFormData.append("rating", data.rating)
+
+        try{
+            const response = await axios({
+                method: "POST",
+                url: 'http://localhost:7000/api/users',
+                //headers: {"Content-Type": "multipart/form-data"},
+                data: userFormData
+            })
+            return response
+        } catch(err) {
+            console.error(err)
+        }
+    }
+
+    const changeHandler = (e: any) => {
+        const value = e.target.value
+        setData({
+            ...data,
+            [e.target.name]: value
+        })
+    }
+
     return(
         
-<form className="sm:px-6">
-  <div className="relative z-0 mb-6 w-full group">
-      <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-      <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
-  </div>
-  <div className="relative z-0 mb-6 w-full group">
-      <input type="text" name="floating_username" id="floating_username" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-      <label htmlFor="floating_username" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Username</label>
-  </div>
-  <div className="relative z-0 mb-6 w-full group">
-      <input type="text" name="floating_description" id="floating_description" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-      <label htmlFor="floating_description" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Channel description</label>
-  </div>
-  <div className="grid md:grid-cols-2 md:gap-6">
-    <div className="relative z-0 mb-6 w-full group">
-        <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-        <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+<form onSubmit={(e) => handleSubmit(e)} className="mx-5 my-5 flex flex-col gap-4">
+    <div>
+        <div className="mb-2 block">
+            <Label
+                htmlFor="email2"
+                value="Your email"
+            />
+        </div>
+        <TextInput
+            id="email2"
+            type="email"
+            name="email"
+            placeholder="name@email.com"
+            value={data.email}
+            onChange={changeHandler}
+            required={true}
+            shadow={true}
+        />
     </div>
-    <div className="relative z-0 mb-6 w-full group">
-        <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-        <label htmlFor="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
+    <div>
+        <div className="mb-2 block">
+            <Label
+                htmlFor="username"
+                value="Your username"
+            />
+        </div>
+        <TextInput
+            id="username"
+            name="username"
+            value={data.username}
+            onChange={changeHandler}
+            required={true}
+            shadow={true}
+        />
     </div>
-  </div>
-  <div className="grid md:grid-cols-2 md:gap-6">
-    <div className="relative z-0 mb-6 w-full group">
-        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
-        <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (123-456-7890)</label>
+    <div>
+        <div className="mb-2 block">
+            <Label
+                htmlFor="description"
+                value="Channel description"
+            />
+        </div>
+        <TextInput
+            id="description"
+            name="description"
+            value={data.description}
+            onChange={changeHandler}
+            required={true}
+            shadow={true}
+        />
     </div>
-    <div className="relative z-0 mb-6 w-full group">
-        <input type="text" name="floating_rating" id="floating_rating" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-        <label htmlFor="floating_rating" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Channel rating (Ex: PG)</label>
+    <div>
+        <div className="mb-2 block">
+            <Label
+                htmlFor="first_name"
+                value="Your name"
+            />
+        </div>
+        <TextInput
+            id="first_name"
+            name="first_name"
+            value={data.first_name}
+            onChange={changeHandler}
+            required={true}
+            shadow={true}
+        />
     </div>
-  </div>
-  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    <div>
+        <div className="mb-2 block">
+            <Label
+                htmlFor="last_name"
+                value="Your last name"
+            />
+        </div>
+        <TextInput
+            id="last_name"
+            name="last_name"
+            value={data.last_name}
+            onChange={changeHandler}
+            required={true}
+            shadow={true}
+        />
+    </div>
+    <div>
+        <div className="mb-2 block">
+            <Label
+                htmlFor="telephone_number"
+                value="Your phone number"
+            />
+        </div>
+        <TextInput
+            id="telephone_number"
+            name="telephone_number"
+            value={data.telephone_number}
+            onChange={changeHandler}
+            required={true}
+            shadow={true}
+        />
+    </div>
+    <div>
+        <div className="mb-2 block">
+            <Label
+                htmlFor="payment_method"
+                value="Payment method"
+            />
+        </div>
+        <TextInput
+            id="payment_method"
+            name="payment_method"
+            value={data.payment_method}
+            onChange={changeHandler}
+            required={true}
+            shadow={true}
+        />
+    </div>
+    <div>
+        <div className="mb-2 block">
+            <Label
+                htmlFor="rating"
+                value="Rating"
+            />
+        </div>
+        <TextInput
+            id="rating"
+            name="rating"
+            value={data.rating}
+            onChange={changeHandler}
+            required={true}
+            shadow={true}
+        />
+    </div>
+    <Button type="submit">
+        Register new account
+    </Button>
 </form>
 
     )
