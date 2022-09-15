ce8d057939607d4a11a160175088fc9c343511fb
Stepalom77 <stepalom22@gmail.com>
2022-09-15 15:59:35 -0500

    Login styles

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,22 +5,22 @@ import { useAuth0 } from '@auth0/auth0-react';
 const Login: React.FC = () => {
     const {loginWithPopup, loginWithRedirect, logout, user, isAuthenticated} = useAuth0()
     return(
-        <div>
+        <div className="ml-40 mt-10 mb-5 grid md:grid-cols-1 md:gap-6 max-w-6xl">
             <h1>Auth0 Login</h1>
-            <ul>
+            <ul className="grid md:grid-cols-1 md:gap-6 max-w-6xl">
                 <li>
-                    <button onClick={loginWithPopup}>Login with Popup</button>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={loginWithPopup}>Login with Popup</button>
                 </li>
                 <li>
-                    <button onClick={loginWithRedirect}>Login with Redirect</button>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={loginWithRedirect}>Login with Redirect</button>
                 </li>
                 <li>
-                    <button onClick={logout as any}>Logout</button>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={logout as any}>Logout</button>
                 </li>
             </ul>
             <h3>User is {isAuthenticated ? "Logged in" : "Not logged in"}</h3>
             {isAuthenticated && (
-                <pre style={{textAlign: 'start'}}>{JSON.stringify(user, null, 2)}</pre>
+                <pre className="text-center">{JSON.stringify(user, null, 2)}</pre>
             )}
         </div>
     )
